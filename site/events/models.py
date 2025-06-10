2ff6e16b7757e093bb6e3d6479ae9fd658ec672b
Jeff <jeff.e.low@gmail.com>
2025-06-10 11:46:10 -0700

    Updates Event Model

--- site/events/models.py
+++ site/events/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Events(models.Model):
     EVENT_TYPES = [
-        ("WNCXP", "WNCXP - Wednesday Night Cyclocross Practice"),
-        ("Women+Clinic", "Women+ (FWTNB) Clinic"),
-        ("LMCX", "Lower Mainland Cyclocross Race"),
-        ("KinderCross", "KinderCross - Kids Cyclocross"),
+        ("wncxp", "WNCXP - Wednesday Night Cyclocross Practice"),
+        ("clinic", "Women+ (FWTNB) Clinic"),
+        ("races", "Lower Mainland Cyclocross Race"),
+        ("kiddiecross", "KiddieCross - Kids Cyclocross"),
     ]
     title = models.CharField(max_length=50)
     website = models.URLField(max_length=200, blank=True)
@@ -13,7 +13,7 @@ class Events(models.Model):
     date = models.DateTimeField()
     location = models.CharField(max_length=200)
     description = models.TextField()
-    type = models.CharField(max_length=50, choices=EVENT_TYPES, default="LMCX")
+    type = models.CharField(max_length=50, choices=EVENT_TYPES, default="races")
     main_image = models.ImageField(upload_to='events/', blank=True, null=True)
     sec_image = models.ImageField(upload_to='events/', blank=True, null=True)
 
