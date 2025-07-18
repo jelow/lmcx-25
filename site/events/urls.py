eda2c9966e243c6e062dab971b0ac3d177fe6958
Jeff <jeff.e.low@gmail.com>
2025-07-17 22:37:19 -0700

    Updates settings to use media directory properly

--- site/events/urls.py
+++ site/events/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views.list_view import EventListView
 from .views.detail_view import EventDetailView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', EventListView.as_view(), name='events_list'),
     path('list/<str:type>/', EventListView.as_view(), name='events_list_by_type'),
     path('<int:pk>/', EventDetailView.as_view(), name='event_detail'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- site/lmcx/urls.py
+++ site/lmcx/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 from events.views.list_view import EventListView
 
@@ -31,3 +33,6 @@ urlpatterns = [
     path('sponsors/', TemplateView.as_view(template_name='base.html'), name='sponsors'),
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
