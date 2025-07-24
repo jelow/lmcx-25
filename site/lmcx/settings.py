5984f6cea4f5d4d1d82398667b1b2b42afdb9efa
Jeff <jeff.e.low@gmail.com>
2025-07-23 23:48:25 -0700

    More settings updates for production. Adds Procfile

--- site/lmcx/settings.py
+++ site/lmcx/settings.py
@@ -18,8 +18,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables from .env file
-env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
-load_dotenv(env_path)
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
