d3280a5411cda550f1fb11d9ec38126f69fcb04f
Jeff <jeff.e.low@gmail.com>
2025-08-06 16:25:46 -0700

    Updates URLs to serve media files in production or debug

--- site/lmcx/urls.py
+++ site/lmcx/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('photos/', TemplateView.as_view(template_name='photos.html'), name='photos'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both development and production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
