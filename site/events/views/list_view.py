e7dce3e3a78c18342ffc00fce779b9f1824cdfea
Jeff <jeff.e.low@gmail.com>
2025-09-23 22:30:20 -0700

    Updates list_view to get the current date every time the function runs, this should fix showing old events.

--- site/events/views/list_view.py
+++ site/events/views/list_view.py
@@ -9,7 +9,7 @@ class EventListView(ListView):
     paginate_by = 10
     context_object_name = "events_list"
     event_type: str = None
-    today: date = date.today()
+    today: date = None
 
     def __init__(self, **kwargs):
         self.event_type = kwargs.get('event_type', None)
@@ -18,7 +18,9 @@ class EventListView(ListView):
 
     def get_queryset(self) -> QuerySet[Events]:
         self.event_type = self.kwargs['type'] if 'type' in self.kwargs else None
+        self.today = date.today()
         if self.event_type is None:
+            # Event type None means it is for the home page, so we show all events upcoming
             return self.model.objects.filter(date__gte=self.today).order_by("date")
         else:
             return self.model.objects.filter(type=self.event_type).order_by("date")
