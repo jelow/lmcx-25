98aa9f5cc8483bc1ae48a28c4996e5175c421801
Jeff <jeff.e.low@gmail.com>
2026-05-22 15:45:31 -0700

    Update Event Model to have 'other' race option, also added field for archiving

--- site/events/models.py
+++ site/events/models.py
@@ -5,7 +5,8 @@ class Events(models.Model):
     EVENT_TYPES = [
         ("wncxp", "WNCXP - Wednesday Night Cyclocross Practice"),
         ("clinic", "Women+ (FWTNB) Clinic"),
-        ("races", "Lower Mainland Cyclocross Race"),
+        ("lmcx_races", "Lower Mainland Cyclocross Race"),
+        ("other_races", "Other Races"),
         ("kindercross", "KinderCross - Kids Cyclocross"),
     ]
     title = models.CharField("Event Name", max_length=50)
@@ -14,9 +15,10 @@ class Events(models.Model):
     date = models.DateTimeField("Event Date")
     location = models.CharField(max_length=200)
     description = HTMLField("Event Description")
-    type = models.CharField("Event Type", max_length=50, choices=EVENT_TYPES, default="races")
+    type = models.CharField("Event Type", max_length=50, choices=EVENT_TYPES, default="lmcx_races")
     main_image = models.ImageField("Main Event Image", upload_to='images/events', blank=True, null=True)
     sec_image = models.ImageField("Icon Image", upload_to='images/events', blank=True, null=True)
+    archived = models.BooleanField("Archived", default=False)
 
     def __str__(self):
         return self.title + " - " + str(self.date.year)
