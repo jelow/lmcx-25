69e35de300bb8bffc7b5dff25ea40945100c98a6
Jeff <jeff.e.low@gmail.com>
2026-05-22 15:50:33 -0700

    Updates view to show all future events for Home kwarg, and all non-archived events for other kwarg (ie. All)

--- site/events/views/list_view.py
+++ site/events/views/list_view.py
@@ -8,22 +8,23 @@ class EventListView(ListView):
     model = Events
     paginate_by = 10
     context_object_name = "events_list"
-    event_type: str = None
+    event_list: str = None
     today: date = None
 
     def __init__(self, **kwargs):
-        self.event_type = kwargs.get('event_type', None)
+        self.event_list = kwargs.get('event_list', None)
         self.template = kwargs.get('template_name', 'events/events_list.html')
         super().__init__(**kwargs)
 
     def get_queryset(self) -> QuerySet[Events]:
-        self.event_type = self.kwargs['type'] if 'type' in self.kwargs else None
+        self.event_list = self.kwargs.get('list', None)
         self.today = date.today()
-        if self.event_type is None:
-            # Event type None means it is for the home page, so we show all events upcoming
+        if self.event_list == 'Home':
+            # Home page: show all upcoming events
             return self.model.objects.filter(date__gte=self.today).order_by("date")
         else:
-            return self.model.objects.filter(type=self.event_type).order_by("date")
+            # Events page: show all non-archived events (even past ones)
+            return self.model.objects.filter(archived=False).order_by("date")
     
     def get_context_data(self, **kwargs) -> Dict:
         context = super().get_context_data(**kwargs)
